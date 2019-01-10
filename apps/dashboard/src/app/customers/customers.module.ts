5eede4513e8645640da85ed7b6e9e06e38036849
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-01-10 12:50:34 -0300

    Adding the app shell. Fill component html

--- apps/dashboard/src/app/customers/customers.module.ts
+++ apps/dashboard/src/app/customers/customers.module.ts
@@ -9,6 +9,9 @@ import { CustomersComponent } from './customers.component';
   imports: [
     CommonModule,
     CustomersRoutingModule
+  ],
+  exports: [
+    CustomersComponent
   ]
 })
 export class CustomersModule { }
